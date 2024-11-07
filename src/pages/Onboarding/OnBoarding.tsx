3fa9de6e008b696b9def98649402a6a0419aa911
Juliana <Julianamoises@alunos.utfpr.edu.br>
2024-11-07 14:35:30 -0300

    changes

--- src/pages/Onboarding/OnBoarding.tsx
+++ src/pages/Onboarding/OnBoarding.tsx
@@ -4,11 +4,7 @@ import { StackNavigationProp } from "@react-navigation/stack";
 import { useNavigation } from "@react-navigation/native";
 
 type RootStackParamList = {
-<<<<<<< HEAD
   Login: undefined;
-=======
-    Home: undefined;
->>>>>>> origin/master
 };
 
 type HomeScreenNavigationProp = StackNavigationProp<RootStackParamList, "Login">;
@@ -29,7 +25,6 @@ const OnBoarding: React.FC = () => {
 
     const navigation = useNavigation<HomeScreenNavigationProp>();
 
-<<<<<<< HEAD
   return (
     <Onboarding
       pages={[
@@ -75,52 +70,6 @@ const OnBoarding: React.FC = () => {
       DoneButtonComponent={DoneButton}
       
     />
-=======
-    return (
-        <Onboarding
-            pages={[
-                {
-                    backgroundColor: "#DBFFD9",
-                    image: (
-                        <Image
-                          style={styles.image}
-                          source={require("../../../assets/foto-onboarding1.png")}
-                        />
-                    ),
-                    title: "Bem Vindo ao Whisp!",
-                    subtitle: "Seu aplicativo de mensagens favorito.",
-                },
-                {
-                    backgroundColor: "#B0D6E0",
-                    image: (
-                      <Image
-                        style={styles.image}
-                        source={require("../../../assets/foto-onboarding2.png")}
-                       />
-                    ),
-                    title: "Conecte-se com pessoas do mundo todo!",
-                    subtitle: "Converse com amigos e familiares.",
-                },
-                {
-                    backgroundColor: "#C5BBE9",
-                    image: (
-                      <Image
-                        style={styles.image}
-                        source={require("../../../assets/foto-onboarding3.png")}
-                      />
-                    ),
-                   title: "Tenha uma experiência incrível!",
-                   subtitle: "Se comunicar nunca foi tão fácil.",
-                },
-         ]}
-        onDone={() => navigation.navigate("Home")}
-        onSkip={() => navigation.navigate("Home")}
-        bottomBarColor="none"
-        NextButtonComponent={NextButton}
-        SkipButtonComponent={DoneButton}
-        DoneButtonComponent={DoneButton}
-     />
->>>>>>> origin/master
   );
 };
 
