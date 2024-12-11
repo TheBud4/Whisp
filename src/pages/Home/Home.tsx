fa4bef9218febee17898f316949761d8058a7a0d
Juliana <Julianamoises@alunos.utfpr.edu.br>
2024-12-11 11:15:35 -0300

    Versão 1.0 dos botões da tela home (somente style ainda nao funcionam) e estrutura da pagina inicial para implementar o código

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,38 @@
 import { Text, View, TouchableOpacity, StyleSheet } from 'react-native'
 import React, { Component } from 'react'
-import * as reactNative from 'react-native';
+import { styles } from 'src/styles/main'
+
+
+
 
 export class Home extends Component {
   render() {
     return (
-      <View>
-        
+      
+      <View style={styles.container}>
+        { /*Conteudo Principal*/}
+        <View style={styles.content}>
+          <Text style={styles.placeholderText}>Aqui onde vai ficar o conteudo Principal</Text>
+        </View>
+       
+       {/*BOTÕES FLUTUATES*/}
+
+       <View style={styles.floatingButtons}>
+        <TouchableOpacity style={styles.button}>
+          {/*Aqui vai o icone de conversas */}
+          <Text style={ styles.buttonLabel}>Conversas</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.button}>
+          {/*Aqui vai o icone de Contatos */}
+          <Text style={ styles.buttonLabel}>Contatos</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.button}>
+          {/*Aqui vai o icone de Grupos */}
+          <Text style={styles.buttonLabel}>Grupos</Text>
+        </TouchableOpacity>
+       </View>
       </View>
     )
   }
