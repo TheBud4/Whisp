f5e834ef66c0aa8869b6af2fa4314a702392a5f7
TheBud4 <murilopmr13@hotmail.com>
2024-12-11 13:46:35 -0300

    refatora tela de Onboarding: substitui botões padrão por um componente customizado e ajusta estilos

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import OnboardingScreen from "./src/pages/Onboarding/OnBoarding";
-import { Home } from "./src/pages/Home/Home";
+import Home  from "./src/pages/Home/Home";
 import Login from "./src/pages/Login/Login";
 import LoginSenha from "./src/pages/Login/LoginSenha";
 
